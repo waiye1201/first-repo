4226058c8a95aac556298c14bfa5c18cf4a0f26c
waiye1201 <waiye1201@gmail.com>
2025-04-02 20:20:52 -0400

    implemented functions from another module in main code

--- mycode.py
+++ mycode.py
@@ -1,4 +1,9 @@
 import arcpy
 from arcpy import env
+
+import my_function_module
+
 env.workspace = r"(C:\Users\yxiao081\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb\OTTAWADATA)"
 featureclass_list=arcpy.ListFeatureClasses()
+for fc in featureclass_list:
+    print(my_function_module.get_number_fields(fc))
