cfd2298cb8f2b505cfb8183db096b59aeb128677
waiye1201 <waiye1201@gmail.com>
2025-04-02 20:07:46 -0400

    geodatabase workspace

--- mycode.py
+++ mycode.py
@@ -1,4 +1,4 @@
 import arcpy
 from arcpy import env
-env.workspace = r"(C:\Users\yxiao081\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb)"
-arcpy.ListDatasets()
+env.workspace = r"(C:\Users\yxiao081\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb\OTTAWADATA)"
+featureclass_list=arcpy.ListFeatureClasses()
